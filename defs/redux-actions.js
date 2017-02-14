c9b8a5e29871545ff65db0f33d8009db4d663ea7
Christoffer Niska <christofferniska@gmail.com>
2017-02-14 21:51:38 +0200

    Update type definitions for redux-actions

--- defs/redux-actions.js
+++ defs/redux-actions.js
@@ -5,10 +5,10 @@ declare module 'redux-actions' {
     type: Type
   ): (payload: Payload) => {type: Type, payload: Payload}
 
-  declare function handleActions(
-    reducerMap: {[key: string]: (state: any, action: {type: string, payload: mixed}) => mixed},
-    defaultState?: mixed
-  ): Function
+  declare function handleActions<State>(
+    reducerMap: {[key: string]: (state: State, action: any) => State},
+    defaultState: State
+  ): (state: State, action: any) => State
 
   declare module.exports: {
     createAction: typeof createAction,
