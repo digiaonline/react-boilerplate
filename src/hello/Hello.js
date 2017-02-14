a9def2f52b2e70b0c5831bff5eb39a598dbc4a0b
Christoffer Niska <christofferniska@gmail.com>
2017-02-14 22:38:44 +0200

    Fix case (part 2)

--- src/hello/Hello.js
+++ src/hello/Hello.js
@@ -5,7 +5,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {sayHello} from './actions'
 import {helloSelector} from './selectors'
-import css from './helloCorrect.css'
+import css from './hello.css'
 
 type HelloProps = {
   message: string,
