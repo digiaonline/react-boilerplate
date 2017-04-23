2d8d35acb383c008156ebc9c54747920ba244dd6
Christoffer Niska <christofferniska@gmail.com>
2017-04-24 01:17:06 +0300

    Add .eslintrc

--- src/hello/selectors.js
+++ src/hello/selectors.js
@@ -1,12 +1,14 @@
-import {createSelector} from 'reselect'
-import type {Selector} from '../../types'
+// @flow
 
-export const getMessage: Selector<string> = (state) =>
+import {createSelector} from 'reselect'
+import type {Selector} from '../types'
+
+export const getMessage: Selector<string, void> = (state) =>
   state.hello.message
 
 export const helloSelector: Selector<any> = createSelector(
   [getMessage],
   (message: string) => ({
-    message
+    message,
   })
 )
