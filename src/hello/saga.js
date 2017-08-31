4e4e34511c681e28ed58c78f6380d1b35f35aca5
Christoffer Niska <christofferniska@gmail.com>
2017-08-31 12:23:34 +0300

    Rebase on latest CRA (fixes #61)

--- src/hello/saga.js
+++ src/hello/saga.js
@@ -1,17 +1,16 @@
 // @flow
 
-import {takeLatest} from 'redux-saga/effects'
-import {fork, put} from 'redux-saga/effects'
+import {all, fork, put, takeLatest} from 'redux-saga/effects'
 import {reply} from './actions'
 
 export function* sayHelloSaga(): Generator<> {
   yield put(reply('Hello from Redux.'))
 }
 
-export default function* (): Generator<> {
-  yield [
-    fork(function* (): Generator<> {
+export default function*(): Generator<> {
+  yield all([
+    fork(function*(): Generator<> {
       yield takeLatest('SAY_HELLO', sayHelloSaga)
     }),
-  ]
+  ])
 }
