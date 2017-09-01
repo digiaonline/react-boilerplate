487288d41d1d90f4a35d66884254d51435618791
Eric Nishio <ericnishio@protonmail.com>
2017-09-01 12:42:58 +0300

    Remove duplicate test

--- src/root/components/Root.test.js
+++ src/root/components/Root.test.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
-import renderer from 'react-test-renderer'
 import Root from './Root'
 
 test('Root component renders without crashing', () => {
   const div = document.createElement('div')
   render(<Root />, div)
-})
-
-test('Root component matches snapshot', () => {
-  const tree = renderer.create(<Root />).toJSON()
-  expect(tree).toMatchSnapshot()
 })
