6bfcbe3354284686a00a5abeb60e59e1bd6bd04e
Eric Nishio <ericnishio@protonmail.com>
2017-09-01 13:47:11 +0300

    Decorate Hello

--- src/hello/components/Hello.js
+++ src/hello/components/Hello.js
@@ -1,9 +1,11 @@
 // @flow
 
 import React, {Component} from 'react'
+import {observer} from 'mobx-react'
 import css from './Hello.css'
 import helloStore from '../store'
 
+@observer
 export class Hello extends Component {
   componentWillMount() {
     helloStore.sayHello()
