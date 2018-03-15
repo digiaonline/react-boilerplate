c7330c081d4299fbe41e1aed30e52cda62a2330a
Ville Vaarala <ville.l.vaarala@gmail.com>
2018-03-15 11:54:59 +0200

    Fix development build and add type checking to builds

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -12,6 +12,7 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 
 // Webpack uses `publicPath` to determine where the app is being served from.
@@ -268,6 +269,7 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new ForkTsCheckerWebpackPlugin(),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
